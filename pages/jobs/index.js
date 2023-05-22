41bbba101f4322d83db12e07e8285984a26e08ee
Daphine Kamusiime <daphnekamusiime09@gmail.com>
2023-05-22 10:06:03 +0300

    fixed server error

---
+++ pages/jobs/index.js
@@ -0,0 +1,84 @@
+    // to fetch data
+    import Link from 'next/link';
+
+    export const getStaticProps = async () => {
+        try {
+          const res = await fetch("http://localhost:1337/api/jobs?populate=*");
+          const data = await res.json();
+      
+          return {
+            props: {
+              getJobs: data,
+            },
+          };
+        } catch (error) {
+          console.error(error);
+          return {
+            props: {
+              getJobs: [],
+            },
+          };
+        }
+      };
+
+      // import Link from 'next/link';
+
+      // ...
+      
+      const JobPage = ({ getJobs }) => {
+        // Minimising the paragraphs on the card component
+        const MAX_LENGTH = 250;
+      
+        return (
+          <ul>
+            <>
+              <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-4/5">
+                <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+                  {getJobs.map((job) => (
+                    <li key={job.key}>
+                      <Link href={'/jobs/' + job.id}>
+                        <div className="text-gray-900 hover:text-purple-700 cursor-pointer">
+                          <h2 className="mb-2 text-xl font-bold leading-snug text-gray-900">
+                            {job.attributes.vacancy}
+                          </h2>
+                        </div>
+                      </Link>
+                      <p className="mb-4 text-sm font-normal text-gray-600">
+                        {job.attributes.description.substring(0, MAX_LENGTH) + ' ...'}
+                      </p>
+                      <Link href="#">
+                        <div className="flex items-center text-gray-700 cursor-pointer">
+                          <div className="avatar">
+                            <img
+                              className="flex-shrink-0 object-cover object-center w-12 h-12 rounded-full"
+                              src={`http://localhost:1337${job.attributes.image.data.attributes.url}`}
+                              alt={'Photo of ' + job.attributes.recruiter}
+                            />
+                          </div>
+                          <div className="ml-2">
+                            <p className="text-sm font-semibold text-gray-900">
+                              {job.attributes.recruiter}
+                            </p>
+                            <p className="text-sm text-gray-600">{job.attributes.email}</p>
+                          </div>
+                        </div>
+                      </Link>
+                    </li>
+                  ))}
+                </div>
+      
+                {/* btns */}
+                <div className="flex flex-col items-center justify-center mt-20 space-x-0 space-y-2 md:space-x-2 md:space-y-0 md:flex-row">
+                  <Link href="/">
+                    <div className="px-3 py-2 text-indigo-500 border border-indigo-500 border-solid hover:text-black md:w-auto cursor-pointer">
+                      Home
+                    </div>
+                  </Link>
+                </div>
+              </section>
+            </>
+          </ul>
+        );
+      };
+      
+      export default JobPage;
